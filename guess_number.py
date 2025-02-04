0b8fa27a2ed9845160f9bf96fc627a8770639d0b
Alexandro Silva <Alexandro18silvas@gmail.com>
2025-02-04 17:22:43 -0300

    commit_3

--- guess_number.py
+++ guess_number.py
@@ -4,16 +4,17 @@ import random
 init()
 yellow = fr.YELLOW
 green = fr.GREEN
-default = fr.WHITE
+white = fr.WHITE
 red = fr.RED
 
-print(f"{yellow}Seja muito bem vindo ao Guess Number do Ale!")
-choice_number = int(input("Digite o número teto do desafio: "))
+print(f"{yellow}Seja muito bem vindo ao Guess Number do Ale!\n")
 
-random_number = random.randint(0, choice_number)
+random_number = random.randint(1, 10)
+n_choices = 0
 
 while True:
-    answer_user = int(input("\nAdvinhe o número: "))
+    answer_user = int(input(f"{white}\nAdvinhe um número de 1 a 10: "))
+    n_choices = n_choices + 1
     if answer_user == random_number:
         print(f"{green}Acertou!")
         break
@@ -21,3 +22,5 @@ while True:
         print(f"{red}Chutou alto, o número aleatório é menor que isso...")
     elif answer_user < random_number:
         print(f"{red}Chutou baixo, o número aleatório é maior que isso...")
+
+print(f"{white}Número de tentativas: {yellow}{n_choices}")
