c4063e50dc87e00b961a0eb9c9f8f4da36907661
Alexandro Silva <Alexandro18silvas@gmail.com>
2025-02-04 17:02:45 -0300

    commit_2

--- guess_number.py
+++ guess_number.py
@@ -1,4 +1,23 @@
+from colorama import init, Fore as fr
 import random
 
-print("Seja muito bem vindo ao Guess Number do Ale!")
-choice_number = input("Digite o número teto do desafio")
+init()
+yellow = fr.YELLOW
+green = fr.GREEN
+default = fr.WHITE
+red = fr.RED
+
+print(f"{yellow}Seja muito bem vindo ao Guess Number do Ale!")
+choice_number = int(input("Digite o número teto do desafio: "))
+
+random_number = random.randint(0, choice_number)
+
+while True:
+    answer_user = int(input("\nAdvinhe o número: "))
+    if answer_user == random_number:
+        print(f"{green}Acertou!")
+        break
+    elif answer_user > random_number:
+        print(f"{red}Chutou alto, o número aleatório é menor que isso...")
+    elif answer_user < random_number:
+        print(f"{red}Chutou baixo, o número aleatório é maior que isso...")
