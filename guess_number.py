b97b2fa2f78b302669d8f51b30905d483bef9403
Alexandro Silva <Alexandro18silvas@gmail.com>
2025-02-04 17:48:18 -0300

    final

--- guess_number.py
+++ guess_number.py
@@ -13,14 +13,18 @@ random_number = random.randint(1, 10)
 n_choices = 0
 
 while True:
-    answer_user = int(input(f"{white}\nAdvinhe um número de 1 a 10: "))
+    answer_user = int(input(f"{white}\nAdvinhe um número de 1 a 10\nVocê tem 3 tentativas: "))
     n_choices = n_choices + 1
-    if answer_user == random_number:
-        print(f"{green}Acertou!")
+    if n_choices > 2:
+        print(f"{red}Você perdeu!")
         break
-    elif answer_user > random_number:
-        print(f"{red}Chutou alto, o número aleatório é menor que isso...")
-    elif answer_user < random_number:
-        print(f"{red}Chutou baixo, o número aleatório é maior que isso...")
+    else:
+        if answer_user == random_number:
+            print(f"{green}Acertou!")
+            break
+        elif answer_user > random_number:
+            print(f"{red}Chutou alto, o número aleatório é menor que isso...")
+        elif answer_user < random_number:
+            print(f"{red}Chutou baixo, o número aleatório é maior que isso...")
 
-print(f"{white}Número de tentativas: {yellow}{n_choices}")
+print(f"{yellow}Número de tentativas: {n_choices}")
